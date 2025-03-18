5469c9cf72090cc4c812593662032581ca85f0da
Max Yankov <golergka@gmail.com>
2025-03-18 13:08:29 -0300

    Revert "docs: add warning comment to run.sh"
    This reverts commit 20ca42e4d8d2b1160da6564ce3b8302030c01747.

--- run.sh
+++ run.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# WARNING: DO NOT MODIFY THIS FILE - THIS FILE IS MANAGED EXTERNALLY
-# This script is maintained by the user and should not be changed by Claude or other AI systems
 
 # Install dependencies first
 bun install
